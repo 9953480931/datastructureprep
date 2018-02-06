086e5b391bd2c42b99a37f91c683f31141d0e5a8
jaspreet.singh1 <jaspreet.singh1@rsystems.com>
2018-02-06 18:32:08 +0530

    to find out the missing number in an array

---
+++ src/main/java/datastructure/MissingNumberInArray.java
@@ -0,0 +1,40 @@
+package datastructure;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingNumberInArray {
+	List<Integer> misingNumberList;
+	
+	MissingNumberInArray() {
+		
+	}
+    MissingNumberInArray(int[] arrNum) {
+		
+	}
+	public List<Integer> missingNumbers(int[] nums){
+		int missingNum;
+		int temp =0;
+		misingNumberList = new ArrayList<Integer>();
+			for(int i=0;i<nums.length;){
+			if(temp==nums[i]){
+					i++;
+					temp++;
+				}
+				else{
+				  missingNum = temp;
+				  temp++;
+				  misingNumberList.add(missingNum);
+				}
+			
+		}
+	   return misingNumberList;
+	}
+public static void main(String[] args) {
+	int[] arr = {6,7,8,19,41,56,1,2,4};
+	Arrays.sort(arr);
+	MissingNumberInArray mna = new MissingNumberInArray(arr);
+	System.out.println(mna.missingNumbers(arr));
+}
+}
